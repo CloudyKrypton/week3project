432e29aa2eff9d23726d1938d34b40ddde1d740b
CloudyKrypton <janetfu04@gmail.com>
2023-10-01 19:46:17 -0400

    Added main code

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
+import static com.mongodb.client.model.Filters.eq;
+import org.bson.Document;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 public class Main {
-    public static void main(String[] args) {
-
-    // TODO: implement code that calls the API from Java.
-
+    public static void main( String[] args ) {
+        // Replace the placeholder with your MongoDB deployment's connection string
+        String uri = "mongodb+srv://janetfu04:<password>@cluster0.uzqaigu.mongodb.net/?retryWrites=true&w=majo" +
+                "rity&appName=AtlasApp\n\n";
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+            MongoCollection<Document> collection = database.getCollection("players");
+            Document doc = collection.find(eq("playername", "John")).first();
+            if (doc != null) {
+                System.out.println(doc.toJson());
+            } else {
+                System.out.println("No matching documents found.");
+            }
+        }
     }
 }
